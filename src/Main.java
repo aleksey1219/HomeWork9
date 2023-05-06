01a5adab17b35620c318b6757e13721be6e2e866
DNS <glotov-aleks@mail.ru>
2023-05-07 01:53:06 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
 
-        System.out.println("Hello world!");
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Решение задачи №1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int current : arr) {
+            sum += current;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+
+    public static void task2() {
+        System.out.println("Решение задачи №2");
+        int[] arr = generateRandomArray();
+        int sum = 0;
     }
 }
